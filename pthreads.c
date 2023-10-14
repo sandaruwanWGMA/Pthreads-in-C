a4c38fd83fb9e8ffe920512e6673eccee496f3f2
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-14 11:48:26 +0530

    commit

--- pthreads.c
+++ pthreads.c
@@ -1,18 +1,26 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 
 #define NUM_THREADS 10
 int thread_idx = 0;
 int sum = 0;               /* this data is shared by the thread(s) */
-void *runner(void *param); /* threads call this function */
+void *runner1(void *param); /* threads call this function */
+void *runner2(void *param);
 int main(int argc, char *argv[])
 {
   pthread_t workers[NUM_THREADS];
   pthread_attr_t attr;
   pthread_attr_init(&attr);
   for (int pt = 0; pt < NUM_THREADS; pt++){
-      pthread_create(&workers[pt], &attr, runner, argv[1]);
+      pthread_create(&workers[pt], &attr, runner1, argv[1]);
+      // sleep(2);
 
   }
   for (int pt = 0; pt < NUM_THREADS; pt++){
@@ -22,11 +30,15 @@ int main(int argc, char *argv[])
   printf("Main thread : %d", sum);
 }
 
-void *runner(void *param){
-  thread_idx++;
-  for (int i = 0; i < 10; i++)
+void *runner1(void *param){
+  for (int i = 0; i < 5000; i++)
   {
     sum++;
   }
-    printf("Thread %d : %d \n", thread_idx, sum);
+  printf("Thread %d : %d \n", pthread_self(), sum);
+  pthread_exit(0);   
+}
+
+void *runner2(void *param){
+    printf("child thread \n");
 }
